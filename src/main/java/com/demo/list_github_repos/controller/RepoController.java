bdc443caa133656b1689d8aad4b806694e751324
Lukasz <lrogowski93@gmail.com>
2023-08-25 13:23:36 +0200

    fix: get username from path variable

--- src/main/java/com/demo/list_github_repos/controller/RepoController.java
+++ src/main/java/com/demo/list_github_repos/controller/RepoController.java
@@ -1,25 +1,22 @@
 package com.demo.list_github_repos.controller;
 
-import com.demo.list_github_repos.controller.request.RepoRequest;
 import com.demo.list_github_repos.controller.response.ErrorResponse;
 import com.demo.list_github_repos.controller.response.RepoResponse;
 import com.demo.list_github_repos.service.RepoService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class RepoController {
     private final RepoService repoService;
 
-    @GetMapping("/repos")
-    public ResponseEntity<?> getRepos(@Valid @RequestBody RepoRequest request){
-        RepoResponse repoResponse = repoService.getRepos(request.username());
+    @GetMapping(value = "/repos/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getRepos(@PathVariable String username){
+        RepoResponse repoResponse = repoService.getRepos(username);
 
         if(repoResponse.status()==HttpStatus.OK.value())
             return ResponseEntity.ok().body(repoResponse.repoList());
--- src/main/java/com/demo/list_github_repos/controller/request/RepoRequest.java
+++
@@ -1,6 +0,0 @@
-package com.demo.list_github_repos.controller.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RepoRequest(@NotBlank(message = "Field username is mandatory") String username) {
-}
