e20897f8b6cbb373789b78fd97496e789e603742
Lukasz <lrogowski93@gmail.com>
2023-08-25 13:27:27 +0200

    feat: validate request accept header, handle exceptions

--- src/main/java/com/demo/list_github_repos/config/CustomExceptionHandler.java
+++ src/main/java/com/demo/list_github_repos/config/CustomExceptionHandler.java
@@ -1,27 +1,20 @@
 package com.demo.list_github_repos.config;
 
 import com.demo.list_github_repos.controller.response.ErrorResponse;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException.Forbidden;
 import org.springframework.web.client.HttpClientErrorException.NotFound;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
-    @ResponseStatus(BAD_REQUEST)
-    @ResponseBody
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        return ErrorResponse.builder()
-                .status(ex.getStatusCode().value())
-                .message(ex.getBindingResult().getAllErrors().get(0).getDefaultMessage())
-                .build();
-    }
 
     @ResponseStatus(NOT_FOUND)
     @ResponseBody
@@ -32,4 +25,28 @@ public class CustomExceptionHandler {
                 .message(ex.getResponseBodyAs(ErrorResponse.class).message())
                 .build();
     }
+
+    @ResponseStatus(NOT_ACCEPTABLE)
+    @ResponseBody
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ResponseEntity<ErrorResponse> mediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException ex) {
+      return ResponseEntity
+              .status(ex.getStatusCode())
+              .contentType(MediaType.APPLICATION_JSON)
+              .body(ErrorResponse.builder()
+                      .status(ex.getStatusCode().value())
+                      .message(ex.getMessage())
+                      .build());
+    }
+
+    @ResponseStatus(FORBIDDEN)
+    @ResponseBody
+    @ExceptionHandler(Forbidden.class)
+    public ErrorResponse forbiddenException(Forbidden ex) {
+        return ErrorResponse.builder()
+                .status(ex.getStatusCode().value())
+                .message(ex.getResponseBodyAs(ErrorResponse.class).message())
+                .build();
+    }
+
 }
